b0a8a5895b80e72a0eb559eb4bf9ed62654b2d06
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2026-04-14 19:56:20 -0600

    fix de los linter rules

--- eslint.config.js
+++ eslint.config.js
@@ -69,7 +69,7 @@ export default tseslint.config(
       'import/no-duplicates': 'error',
       'prettier/prettier': 'error',
       'local-rules/header-comment': 'error',
-      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
     },
   },
   prettier
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,11 +6,6 @@
  */
 
 /// <reference types="vite/client" />
- * Tecnológico de Monterrey — Campus Chihuahua
- * Desarrollo e Implantación de Sistemas de Software
- * TC3005B GPO500 - 2026
- * Autozone QA Automation
- */
 
 interface ImportMetaEnv {
   readonly VITE_API_URL_LOCAL: string
