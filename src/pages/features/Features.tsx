d9265fdb1e62c7829f3ac0e55d9df0f64db80e29
YH <main@relay.yats.dev>
2026-04-24 17:56:02 -0600

    QAZ3-15: No features message

--- src/pages/features/Features.tsx
+++ src/pages/features/Features.tsx
@@ -5,6 +5,7 @@
  * Autozone QA Automation
  */
 
+import { Text } from '@mantine/core'
 import { TitleHeader } from '@/components/layout/TitleHeader/TitleHeader'
 import { ConnectingDatabasePanel } from '@/components/ui/HandleStates/ConnectingDatabasePanel'
 import { ErrorPanel } from '@/components/ui/HandleStates/ErrorPanel'
@@ -38,6 +39,11 @@ export function Features() {
       {isLoading && features.length === 0 && <ConnectingDatabasePanel />}
       {error && <ErrorPanel error={error} />}
       {features.length > 0 && !isLoading && !error && <FeaturesList data={features} />}
+      {!isLoading && features.length === 0 && (
+        <Text ta="center" c="#8C8C94" mt="xl" size="xl">
+          No Features available
+        </Text>
+      )}
     </div>
   )
 }
