f9904f4d1ae3fd8c372ac29f09ad8ee8b65c9c89
DiegoSanchezTec <A01563558@tec.mx>
2026-05-10 12:05:12 -0600

    Etiqueta a un boton para QA LeerTestCases
    hola plis acepten es una linea

--- src/pages/tests-cases/TestCases.tsx
+++ src/pages/tests-cases/TestCases.tsx
@@ -65,7 +65,7 @@ export function TestCases() {
     <div>
       <Modal.Root opened={opened} onClose={handleClose}>
         <Modal.Overlay />
-        <Modal.Content>
+        <Modal.Content data-testid="testcase-modal">
           <Modal.Header>
             <Modal.Title fw={700} mb="lg" c="#1A1A1F">
               {selectedTestCase?.title}
